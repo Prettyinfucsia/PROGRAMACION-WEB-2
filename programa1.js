b191b1a52199acbafc0c3af23b1947fa0d6fbf0c
Prettyinfucsia <jaquelinemendieta44@aragon.unam.mx>
2026-02-25 23:49:02 -0600

    Add files via upload

---
+++ programa1.js
@@ -0,0 +1,15 @@
+function ejecutarPrograma1() {
+
+    let nombre = prompt("Ingresa tu nombre:");
+    let edad = prompt("Ingresa tu edad:");
+    let carrera = prompt("Ingresa tu carrera:");
+    let correo = prompt("Ingresa tu correo electrónico:");
+
+    alert(
+        "Datos ingresados:\n" +
+        "Nombre: " + nombre + "\n" +
+        "Edad: " + edad + "\n" +
+        "Carrera: " + carrera + "\n" +
+        "Correo: " + correo
+    );
+}
