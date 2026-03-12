f495a852926ca1b97a5d78271f9a7ab97e26b55b
Prettyinfucsia <jaquelinemendieta44@aragon.unam.mx>
2026-03-12 15:35:08 -0600

    Datos

--- programa1.js
+++ programa1.js
@@ -1,16 +1,38 @@
-function ejecutarPrograma1() {
+function ejecutarPrograma1(){
 
-let nombre = prompt("Ingresa tu nombre:");
-let edad = prompt("Ingresa tu edad:");
-let carrera = prompt("Ingresa tu carrera:");
-let correo = prompt("Ingresa tu correo electrónico:");
+document.getElementById("areaPrograma").innerHTML =
 
-alert(
-"Datos ingresados:\n" +
-"Nombre: " + nombre + "\n" +
-"Edad: " + edad + "\n" +
-"Carrera: " + carrera + "\n" +
-"Correo: " + correo
-);
+`<h3>Registro de datos</h3>
+
+Nombre: <input type="text" id="nombre"><br><br>
+
+Edad: <input type="number" id="edad"><br><br>
+
+Carrera: <input type="text" id="carrera"><br><br>
+
+Correo: <input type="email" id="correo"><br><br>
+
+<button onclick="mostrarDatos()">Guardar</button>
+
+<div id="salida"></div>
+`;
+
+}
+
+function mostrarDatos(){
+
+let nombre=document.getElementById("nombre").value;
+let edad=document.getElementById("edad").value;
+let carrera=document.getElementById("carrera").value;
+let correo=document.getElementById("correo").value;
+
+document.getElementById("salida").innerHTML=
+
+`<h4>Datos ingresados</h4>
+
+Nombre: ${nombre} <br>
+Edad: ${edad} <br>
+Carrera: ${carrera} <br>
+Correo: ${correo}`;
 
 }
