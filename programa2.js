e2db79b32a5fb31826a3623572d0f2a775eeb310
Prettyinfucsia <jaquelinemendieta44@aragon.unam.mx>
2026-03-12 15:35:36 -0600

    operaciones

--- programa2.js
+++ programa2.js
@@ -1,21 +1,38 @@
-function ejecutarPrograma2() {
+function ejecutarPrograma2(){
 
-let num1 = parseFloat(prompt("Ingresa el primer número:"));
-let num2 = parseFloat(prompt("Ingresa el segundo número:"));
+document.getElementById("areaPrograma").innerHTML =
 
-let suma = num1 + num2;
-let resta = num1 - num2;
-let multiplicacion = num1 * num2;
-let division = num1 / num2;
-let modulo = num1 % num2;
+`<h3>Operaciones matemáticas</h3>
 
-alert(
-"Resultados:\n" +
-"Suma: " + suma + "\n" +
-"Resta: " + resta + "\n" +
-"Multiplicación: " + multiplicacion + "\n" +
-"División: " + division + "\n" +
-"Módulo: " + modulo
-);
+Número 1: <input type="number" id="num1"><br><br>
 
+Número 2: <input type="number" id="num2"><br><br>
+
+<button onclick="calcular()">Calcular</button>
+
+<div id="salida"></div>
+`;
+
+}
+
+function calcular(){
+
+let num1=parseFloat(document.getElementById("num1").value);
+let num2=parseFloat(document.getElementById("num2").value);
+
+let suma=num1+num2;
+let resta=num1-num2;
+let multiplicacion=num1*num2;
+let division=num1/num2;
+let modulo=num1%num2;
+
+document.getElementById("salida").innerHTML=
+
+`<h4>Resultados</h4>
+
+Suma: ${suma}<br>
+Resta: ${resta}<br>
+Multiplicación: ${multiplicacion}<br>
+División: ${division}<br>
+Módulo: ${modulo}`;
 }
