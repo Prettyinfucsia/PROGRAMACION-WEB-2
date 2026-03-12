9c83d89de7b0ebbdfc092bfa2e8f61cfce9f813f
Prettyinfucsia <jaquelinemendieta44@aragon.unam.mx>
2026-03-12 15:22:55 -0600

    Programacion_web_2

--- programa2.js
+++ programa2.js
@@ -1,20 +1,21 @@
 function ejecutarPrograma2() {
 
-    let num1 = parseFloat(prompt("Ingresa el primer número:"));
-    let num2 = parseFloat(prompt("Ingresa el segundo número:"));
+let num1 = parseFloat(prompt("Ingresa el primer número:"));
+let num2 = parseFloat(prompt("Ingresa el segundo número:"));
 
-    let suma = num1 + num2;
-    let resta = num1 - num2;
-    let multiplicacion = num1 * num2;
-    let division = num1 / num2;
-    let modulo = num1 % num2;
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+let modulo = num1 % num2;
 
-    alert(
-        "Resultados:\n" +
-        "Suma: " + suma + "\n" +
-        "Resta: " + resta + "\n" +
-        "Multiplicación: " + multiplicacion + "\n" +
-        "División: " + division + "\n" +
-        "Módulo: " + modulo
-    );
+alert(
+"Resultados:\n" +
+"Suma: " + suma + "\n" +
+"Resta: " + resta + "\n" +
+"Multiplicación: " + multiplicacion + "\n" +
+"División: " + division + "\n" +
+"Módulo: " + modulo
+);
+
 }
