0a590e03e4b39e1fbb986e1f05da0d0b11f8ed5c
Prettyinfucsia <jaquelinemendieta44@aragon.unam.mx>
2026-03-12 15:41:03 -0600

    operaciones lógicas

--- programa3.js
+++ programa3.js
@@ -1,44 +1,49 @@
-let numeroSecreto = Math.floor(Math.random()*10)+1;
-let intentos = 0;
-
 function ejecutarPrograma3(){
 
 document.getElementById("areaPrograma").innerHTML =
 
-`<h3>Adivina el número (1-10)</h3>
+`<h3>Operaciones Lógicas</h3>
 
-<input type="number" id="intento">
+Valor A: <select id="valorA">
 
-<button onclick="adivinar()">Intentar</button>
+<option value="true">Verdadero</option>
+<option value="false">Falso</option>
+</select>
+
+<br><br>
+
+Valor B: <select id="valorB">
+
+<option value="true">Verdadero</option>
+<option value="false">Falso</option>
+</select>
+
+<br><br>
+
+<button onclick="calcularLogica()">Calcular</button>
 
 <div id="salida"></div>
 `;
 
 }
 
-function adivinar(){
+function calcularLogica(){
 
-let intento=parseInt(document.getElementById("intento").value);
+let A = document.getElementById("valorA").value === "true";
+let B = document.getElementById("valorB").value === "true";
 
-intentos++;
+let AND = A && B;
+let OR = A || B;
+let NOTA = !A;
+let NOTB = !B;
 
-if(intento > numeroSecreto){
+document.getElementById("salida").innerHTML =
 
-document.getElementById("salida").innerHTML="El número es menor";
+`<h4>Resultados</h4>
 
-}
-
-else if(intento < numeroSecreto){
-
-document.getElementById("salida").innerHTML="El número es mayor";
-
-}
-
-else{
-
-document.getElementById("salida").innerHTML=
-`¡Correcto! Lo lograste en ${intentos} intentos`;
-
-}
+A AND B = ${AND} <br>
+A OR B = ${OR} <br>
+NOT A = ${NOTA} <br>
+NOT B = ${NOTB}`;
 
 }
