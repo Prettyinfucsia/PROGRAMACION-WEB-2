5991c97f73529f87ff42a381b66a8584876a33e4
Prettyinfucsia <jaquelinemendieta44@aragon.unam.mx>
2026-03-12 15:22:21 -0600

    Programacion_web_2

--- programa1.js
+++ programa1.js
@@ -1,15 +1,16 @@
 function ejecutarPrograma1() {
 
-    let nombre = prompt("Ingresa tu nombre:");
-    let edad = prompt("Ingresa tu edad:");
-    let carrera = prompt("Ingresa tu carrera:");
-    let correo = prompt("Ingresa tu correo electrónico:");
+let nombre = prompt("Ingresa tu nombre:");
+let edad = prompt("Ingresa tu edad:");
+let carrera = prompt("Ingresa tu carrera:");
+let correo = prompt("Ingresa tu correo electrónico:");
 
-    alert(
-        "Datos ingresados:\n" +
-        "Nombre: " + nombre + "\n" +
-        "Edad: " + edad + "\n" +
-        "Carrera: " + carrera + "\n" +
-        "Correo: " + correo
-    );
+alert(
+"Datos ingresados:\n" +
+"Nombre: " + nombre + "\n" +
+"Edad: " + edad + "\n" +
+"Carrera: " + carrera + "\n" +
+"Correo: " + correo
+);
+
 }
